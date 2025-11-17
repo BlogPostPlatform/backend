70b73e4ac59123846acee0ef7c935766b677bb6f
bahodir <vbahodir00@gmail.com>
2025-11-17 15:48:36 +0500

    fixed urls.py with prepended api path

--- core/urls.py
+++ core/urls.py
@@ -22,9 +22,9 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("ckeditor/", include("ckeditor_uploader.urls")),
+    path("api/admin/", admin.site.urls),
+    path("api/schema/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/docs/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/ckeditor/", include("ckeditor_uploader.urls")),
     path("api/", include("apps.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
