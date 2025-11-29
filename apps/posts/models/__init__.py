8006dd811ebca701ed183f95d909e83784ea0e21
bahodir <vbahodir00@gmail.com>
2025-11-29 19:45:38 +0500

    updating ci/cd v2

--- apps/posts/models/__init__.py
+++ apps/posts/models/__init__.py
@@ -2,3 +2,10 @@ from .posts import Post
 from .files import PostImage
 from .reactions import ReactionType
 from .reactions import Reaction
+
+__all__ = [
+    "Post",
+    "PostImage",
+    "Reaction",
+    "ReactionType",
+]
